4a9b86e02d07bfacf80df7e37767486d9e54167b
Chorling <chorlinglau@qq.com>
2024-04-25 18:29:12 +0800

    0425update

--- src/uot/eval.py
+++ src/uot/eval.py
@@ -3,19 +3,20 @@ import json
 
 def evaluate_performance(file, task):
     cnt = success = 0
-    efficiency = success_efficiency = 0
+    length = success_length = 0
     with (open(file, 'r') as f):
         data = json.load(f)
     for i in data:
         cnt += 1
         if i['state'] == 1:
             success += 1
-            success_efficiency += i['turn']
-            efficiency += i['turn']
+            success_length += i['turn']
+            length += i['turn']
         else:
-            efficiency += task.max_turn
+            length += task.max_turn
 
     print('Dialogue Count:', cnt)
     print('Success Rate:', success / cnt)
-    print('Efficiency in Successful Cases:', success_efficiency / success)
-    print('Efficiency:', efficiency / cnt)
+    print('Mean Conversation Length in Successful Cases:', success_length / success)
+    print('Mean Conversation Length:', length / cnt)
+
